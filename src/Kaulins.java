b49d1438b5494fbfb33941d173a2432dcaadbe5c
antans <antanswork@gmail.com>
2022-01-18 10:18:12 +0200

    Labojums random

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,10 +3,10 @@ import java.util.Random;
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int s;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		s = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+s);
 	}
 
 }
