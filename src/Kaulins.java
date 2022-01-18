88b1f17e3d25b069664d55a7671a010272bfc726
antans <antanswork@gmail.com>
2022-01-18 10:37:25 +0200

    Ieviesta metode

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,12 +1,15 @@
 import java.util.Random;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
+	static void mestkaulinu(){
 		int s;
 		Random rand = new Random();
 		s = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+s);
+	}
+
+	public static void main(String[] args) {
+		mestkaulinu();
 	}
 
 }
