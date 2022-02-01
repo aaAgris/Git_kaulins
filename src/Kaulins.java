1e086d20dcd3d045d423a6631b1d250d1cdaff7c
antans <antanswork@gmail.com>
2022-02-01 10:42:05 +0200

    Parada lielaako uzmesto kaulinu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -7,16 +7,20 @@ public class Kaulins {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		int s;
+		int s,lielakais=0;
 		System.out.println("Cik reizes mest kauliòu?");
 		int reizes = scan.nextInt();
 		Random rand = new Random();
 		for(int i=1;i<=reizes;i++){
 		s = rand.nextInt(5)+2;
 		System.out.println("Uzkrita skaitlis: "+s);
+		if(s>lielakais){
+			lielakais = s;
+		}
 		
 		
 	}
+		System.out.println("Lielakais skaitlis ir : "+lielakais);
 	}
 
 }
